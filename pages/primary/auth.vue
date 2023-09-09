1ac3453a9131abac30ae6d037be06dbfefd2a699
Kelvin Njiraini <njirainikevo46@gmail.com>
2023-09-09 17:57:29 +0300

    feat: element plus validation

--- pages/primary/auth.vue
+++ pages/primary/auth.vue
@@ -17,13 +17,17 @@
             </h2>
             <!-- Actual form -->
             <el-form
-                @submit.prevent="onSubmit"
+                ref="ruleFormRef"
+                @submit.prevent="onSubmit(ruleFormRef)"
                 label-position="top"
                 label-width="100px"
+                :rules="rules"
+                :model="initialState"
             >
                 <div class="flex flex-col text-slate-600 mt-4">
                     <el-form-item
                         label="First Name"
+                        prop="firstName"
                         v-if="authState === 'signup'"
                     >
                         <el-input
@@ -31,12 +35,13 @@
                             name="firstName"
                             id="firstName"
                             placeholder="Enter your first name"
-                            v-model="firstName"
+                            v-model="initialState.firstName"
                             size="large"
                         />
                     </el-form-item>
                     <el-form-item
                         label="Last Name"
+                        prop="lastName"
                         v-if="authState === 'signup'"
                     >
                         <el-input
@@ -44,18 +49,19 @@
                             name="lastName"
                             id="lastName"
                             placeholder="Enter your first name"
-                            v-model="lastName"
+                            v-model="initialState.lastName"
                             size="large"
                         />
                     </el-form-item>
                     <el-form-item
                         label="Level Of Education"
+                        prop="levelOfEducation"
                         v-if="authState === 'signup'"
                     >
                         <el-select
                             name="levelOfEducation"
                             id="levelOfEducation"
-                            v-model="levelOfEducation"
+                            v-model="initialState.levelOfEducation"
                             size="large"
                             class="w-full"
                         >
@@ -68,49 +74,49 @@
                             </el-option>
                         </el-select>
                     </el-form-item>
-                    <el-form-item label="Email address">
+                    <el-form-item label="Email address" prop="email">
                         <!-- <label for="email" class="text-base">Email</label> -->
                         <el-input
                             type="email"
                             name="email"
                             id="email"
                             placeholder="Enter your email address"
-                            v-model="email"
+                            v-model="initialState.email"
                             size="large"
                         />
                     </el-form-item>
 
-                    <el-form-item label="Password">
+                    <el-form-item label="Password" prop="password">
                         <!-- <label for="password" class="text-base">Password</label> -->
                         <el-input
                             type="password"
                             name="password"
                             id="password"
                             placeholder="Enter your password"
-                            v-model="password"
+                            v-model="initialState.password"
                             size="large"
                         />
                     </el-form-item>
 
                     <!-- form actions -->
                     <div>
-                        <el-button
+                        <button
                             class="outline-none bg-purple-800 text-white hover:bg-purple-500 px-5 py-2 rounded-lg"
-                            native-type="submit"
+                            type="submit"
                         >
                             {{ authState === 'login' ? 'Login' : 'Signup' }}
-                        </el-button>
-                        <el-button
+                        </button>
+                        <button
                             class="outline-none underline hover:text-purple-500 ml-3 cursor-pointer"
                             @click="changeAuthState"
-                            native-type="button"
+                            type="button"
                         >
                             {{
                                 authState === 'login'
                                     ? 'or Sign up'
                                     : 'or Login'
                             }}
-                        </el-button>
+                        </button>
                     </div>
                 </div>
             </el-form>
@@ -120,19 +126,75 @@
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
-import { createUser } from '@/server/database/userRepository';
+import { useRegister } from '@/composables/useRegister';
+import { FormRules, FormInstance } from 'element-plus';
+import { RegisterRule } from '@/utils/types';
 const { query } = useRoute();
 
 const client = useSupabaseClient();
 const user = useSupabaseUser();
 
 const authState = ref('login');
-const email = ref('');
-const password = ref('');
+
+const initialState = reactive<RegisterRule>({
+    email: '',
+    firstName: '',
+    lastName: '',
+    levelOfEducation: '',
+    password: '',
+});
 const isLoading = ref(false);
-const firstName = ref('');
-const lastName = ref('');
-const levelOfEducation = ref('');
+const err = ref<string | null>(null);
+
+const ruleFormRef = ref<FormInstance>();
+const rules = reactive<FormRules<RegisterRule>>({
+    email: [
+        { required: true, message: 'Email field is required', trigger: 'blur' },
+        {
+            type: 'email',
+            message: 'Email format is not correct',
+            trigger: 'blur',
+        },
+    ],
+    firstName: [
+        {
+            required: true,
+            message: 'First name field is required',
+            trigger: 'change',
+        },
+    ],
+    lastName: [
+        {
+            required: true,
+            message: 'Last name field is required',
+            trigger: 'change',
+        },
+    ],
+    levelOfEducation: [
+        {
+            required: true,
+            message: 'Level of education field is required.',
+            trigger: 'change',
+        },
+        {
+            enum: ['primary', 'high-school'],
+            message:
+                'Level of education can only be "high school" or "primary"',
+        },
+    ],
+    password: [
+        {
+            required: true,
+            message: 'Password field is required',
+            trigger: 'change',
+        },
+        {
+            min: 6,
+            message: 'Password length has to be more than 6 characters',
+            trigger: 'change',
+        },
+    ],
+});
 
 function changeAuthState() {
     if (authState.value === 'login') {
@@ -142,26 +204,46 @@ function changeAuthState() {
     authState.value = 'login';
 }
 
-function onSubmit() {
-    if (authState.value === 'signup') {
-        signup();
-        return;
-    }
-    // signin function
-    login();
+async function onSubmit(formEl: FormInstance | undefined) {
+    if (!formEl) return;
+    await formEl.validate((valid, fields) => {
+        if (valid) {
+            if (authState.value === 'signup') {
+                signup();
+                return;
+            }
+            // signin function
+            login();
+        } else {
+            console.log('error submit!', valid, fields);
+        }
+    });
 }
 
 const signup = async () => {
+    err.value = null;
+
     isLoading.value = true;
     try {
+        const userData = {
+            email: initialState.email,
+            firstName: initialState.firstName,
+            lastName: initialState.lastName,
+            levelOfEducation: initialState.levelOfEducation,
+        };
+        const response = await useRegister(userData);
+        if (response.error) {
+            throw response.error;
+        }
+
         const { data, error } = await client.auth.signUp({
-            email: email.value,
-            password: password.value,
+            email: initialState.email,
+            password: initialState.password,
             options: {
                 data: {
-                    firstName: firstName.value,
-                    lastName: lastName.value,
-                    levelOfEducation: levelOfEducation.value,
+                    firstName: initialState.firstName,
+                    lastName: initialState.lastName,
+                    levelOfEducation: initialState.levelOfEducation,
                 },
             },
         });
@@ -169,34 +251,6 @@ const signup = async () => {
             throw error;
         }
         console.log(data);
-        if (data && data.user) {
-            const { email: email_address } = data.user;
-            const {
-                firstName: first_name,
-                lastName: last_name,
-                levelOfEducation: level_of_education,
-            } = data.user.user_metadata;
-
-            if (
-                email_address &&
-                first_name &&
-                last_name &&
-                level_of_education
-            ) {
-                const userData = {
-                    email: email_address,
-                    firstName: first_name,
-                    lastName: last_name,
-                    levelOfEducation: level_of_education,
-                };
-                const newUser = await useFetch('/api/auth/signup', {
-                    method: 'POST',
-                    body: userData,
-                });
-                console.log(newUser);
-            }
-        }
-        // if (error) console.log(error);
     } catch (err) {
         console.log(err);
     }
@@ -205,8 +259,8 @@ const signup = async () => {
 const login = async () => {
     isLoading.value = true;
     const { data, error } = await client.auth.signInWithPassword({
-        email: email.value,
-        password: password.value,
+        email: initialState.email,
+        password: initialState.password,
     });
 
     console.log(data);
--- utils/types.ts
+++ utils/types.ts
@@ -4,3 +4,11 @@ export type UserData = {
     lastName: string;
     levelOfEducation: string;
 };
+
+export interface RegisterRule {
+    firstName: string;
+    lastName: string;
+    levelOfEducation: string;
+    email: string;
+    password: string;
+}
